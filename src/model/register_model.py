44d5e1deea3ba28e5efa9aea75917608ecdfa337
kush10521 <kushsingh10521@gmail.com>
2026-08-12 16:18:13 +0530

    add key based authentication

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,8 +6,21 @@ import logging
 import os
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/kush10521/mlops-mini-project.mlflow')
-dagshub.init(repo_owner='kush10521', repo_name='mlops-mini-project', mlflow=True)
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "kush10521"
+repo_name = "mlops-mini-project"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}/mlflow")
 
 
 # logging configuration
