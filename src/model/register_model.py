2c8ad3e220677586079f3c44a5b7df6258f53799
kush10521 <kushsingh10521@gmail.com>
2026-08-12 16:34:59 +0530

    corrected minor bug

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -12,7 +12,7 @@ dagshub_token = os.getenv("DAGSHUB_PAT")
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = "kush10521"
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
